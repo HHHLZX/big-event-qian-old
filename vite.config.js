761d88e569980452aaa74bf971d78ed06c0a76ec
HHHLZX <138979515+HHHLZX@users.noreply.github.com>
2024-10-31 09:11:04 +0800

    配置gh-pages打包配置

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [vue(),],
-    base: '/big-event-qian/', // 替换为你的 GitHub 仓库名
+    // base: '/big-event-qian/', // 替换为你的 GitHub 仓库名
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url))
